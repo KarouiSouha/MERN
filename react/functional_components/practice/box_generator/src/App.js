4c9cc8f849ffcbf6426201d8c69216cf36519996
karoui souha <karoui.souha18@gmail.com>
2023-06-23 16:33:21 +0200

    Box generator

---
+++ react/functional_components/practice/box_generator/src/App.js
@@ -0,0 +1,18 @@
+import Boxgenerator from "./composents/Boxgenerator";
+import { useState } from "react";
+import Form from "./composents/Form";
+function App() {
+  const [colors,setcolor]=useState([{color:"red",width:'100'},{color:"blue",width:'100'},{color:"purple",width:'100'}])
+  
+  return (
+    <div className="App">
+      <Form setcolor={setcolor} colors={colors}  />
+      {colors.map((color,idx)=>{
+        return <Boxgenerator color={color} key={idx} />
+      })}
+
+    </div>
+  );
+}
+
+export default App;
