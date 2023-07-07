6ad5cb0c2403dd9f6a46dda515b985db9273db3d
karoui souha <karoui.souha18@gmail.com>
2023-07-07 18:03:22 +0200

    Product Manager (Part II)

--- fullstack_mern/product_manager1/client/src/App.js
+++ fullstack_mern/product_manager1/client/src/App.js
@@ -1,10 +1,17 @@
 import Form from './components/Form';
+import ShowOne from './components/showoneproduct';
+import Show from './components/showproduct';
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
     <Form/>
+    <Routes>
+    <Route path='/' element={<Show />} />
+    <Route path='/products/:id' element={<ShowOne />} />
+    </Routes>
     </div>
   );
 }
